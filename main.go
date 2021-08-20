65194f8489d6942c6e6326773915d960709c5c41
JINSU SANG <48817731+jinsuSang@users.noreply.github.com>
2021-08-20 13:33:09 +0900

    delete unused functions

--- main.go
+++ main.go
@@ -7,16 +7,6 @@ import (
 	"strings"
 )
 
-type StringArray []string
-
-func (arr *StringArray) String() string {
-	return fmt.Sprintf("%v", *arr)
-}
-
-func (arr *StringArray) Set(s string) error {
-	*arr = strings.Split(s, ",")
-	return nil
-}
 
 func main() {
 
