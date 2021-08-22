75cf940c3e3c52b1df5126e16028d2d035470ce4
jinsuSang <climbplant39@kakao.com>
2021-08-22 19:57:08 +0900

    feat: add AddNotes function

--- notes/notes.go
+++ notes/notes.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
 )
 
@@ -12,7 +13,29 @@ type Note struct {
 	Body  []string `json:"body"`
 }
 
-func SaveNotes(notes Notes) {
+func AddNotes(title, body string) {
+	notes := loadNotes()
+
+	found := -1
+	for i, n := range notes {
+		if n.Title == title {
+			found = i
+			break
+		}
+	}
+
+	if found == -1 {
+		notes = append(notes, Note{Title: title, Body: []string{body}})
+		fmt.Println("New note added")
+	} else {
+		fmt.Println("Note is already existed")
+		notes[found].Body = append(notes[found].Body, body)
+		fmt.Println("body added")
+	}
+	saveNotes(notes)
+}
+
+func saveNotes(notes Notes) {
 	f, err := os.OpenFile("notes.json", os.O_WRONLY, 0755)
 	if err != nil {
 		if os.IsNotExist(err) {
@@ -35,7 +58,7 @@ func SaveNotes(notes Notes) {
 	}
 }
 
-func LoadNotes() Notes {
+func loadNotes() Notes {
 	f, err := os.OpenFile("notes.json", os.O_RDONLY, 0755)
 	if err != nil {
 		panic(err)
@@ -49,12 +72,19 @@ func LoadNotes() Notes {
 
 	encoder := json.NewDecoder(f)
 
-	var notes Notes
-	err = encoder.Decode(&notes)
+	notes := new(Notes)
+	err = encoder.Decode(notes)
 	if err != nil {
 		panic(err)
 	}
-
-	return notes
+	return *notes
 }
 
+func main() {
+	note := Note{Title: "hello", Body: []string{"hello2"}}
+	var notes Notes
+	notes = []Note{note, {Title: "hello2", Body: []string{"hello3"}}}
+	saveNotes(notes)
+
+	AddNotes("hello", "bye2")
+}
